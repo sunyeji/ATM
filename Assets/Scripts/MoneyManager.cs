22da4866ca248351c64d306d41551a2f6bccae90
sunyeji <ssuu0167@gmail.com>
2025-06-10 15:31:49 +0900

    add : 파일
    기능 정리

--- Assets/Scripts/MoneyManager.cs
+++ Assets/Scripts/MoneyManager.cs
@@ -4,15 +4,98 @@ using UnityEngine;
 
 public class MoneyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   public void DepositAmount(int amount)  //입금
     {
+        int money = int.Parse(GameManager.Instance.moneyText.text.Replace(",", ""));  //마지막 문구는 문자열 쉼표제거
+        int coin = int.Parse(GameManager.Instance.coinText.text.Replace(",", ""));
         
-    }
+        if (money >= amount) 
+        {
+            money -= amount;
+            coin += amount;
 
-    // Update is called once per frame
-    void Update()
+
+            GameManager.Instance.userData.money = money;
+            GameManager.Instance.userData.coin = coin;
+            Debug.Log("입금 되었습니다.");
+            
+            UserManager.Instance.SaveData();
+            GameManager.Instance.Refresh();
+        }
+        else
+        {
+            PopupBtn.Instance.PupupError.SetActive(true);
+            Debug.Log("잔액이 부족합니다.");
+        }
+    }
+    
+    
+    public void CostomDeposit() //자유입금
     {
-        
+        int isinputDeposit = int.Parse(PopupBtn.Instance.inputDeposit.text);
+
+        if (GameManager.Instance.userData.money >= isinputDeposit)
+        {
+            GameManager.Instance.userData.money -= isinputDeposit;
+            GameManager.Instance.userData.coin += isinputDeposit;
+            Debug.Log("자유입금 되었습니다.");
+            
+            UserManager.Instance.SaveData();
+            GameManager.Instance.Refresh();
+        }
+
+        else
+        {
+            PopupBtn.Instance.PupupError.SetActive(true);
+            Debug.Log("잔액이 부족합니다.");
+        }
+    }
+    
+    
+    public void WithdrawalAmount(int amount) //출금
+    {
+        int money = int.Parse(GameManager.Instance.moneyText.text.Replace(",", ""));
+        int coin = int.Parse(GameManager.Instance.coinText.text.Replace(",", ""));
+
+        if (coin >= amount)
+        {
+            money += amount;
+            coin -= amount;
+            
+            GameManager.Instance.userData.coin = coin;
+            GameManager.Instance.userData.money = money;
+            Debug.Log("출금 되었습니다.");
+            
+            UserManager.Instance.SaveData();
+            GameManager.Instance.Refresh();
+            
+        }
+        else
+        {
+            PopupBtn.Instance.PupupError.SetActive(true);
+            Debug.Log("잔액이 부족합니다.");
+        }
+    }
+    
+    public void CostomWithdrawal() //자유 출금
+    {
+        int isinputWithdrawal = int.Parse(PopupBtn.Instance.inputWithdrawal.text);
+        if (GameManager.Instance.userData.coin >= isinputWithdrawal)
+        {
+            GameManager.Instance.userData.money += isinputWithdrawal;
+            GameManager.Instance.userData.coin -= isinputWithdrawal;
+            Debug.Log("자유출금 되었습니다.");
+            
+            UserManager.Instance.SaveData();
+            GameManager.Instance.Refresh();
+            
+        }
+
+        else
+        {
+            
+            PopupBtn.Instance.PupupError.SetActive(true);
+            Debug.Log("잔액이 부족합니다.");
+        }
     }
 }
--- Assets/Scripts/PopupBtn.cs
+++ Assets/Scripts/PopupBtn.cs
@@ -6,6 +6,20 @@ using UnityEngine.UI;
 
 public class PopupBtn : MonoBehaviour
 {
+    public static PopupBtn Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
     public UserData userData;
     
     [Header("입출금 판넬 버튼들")]
@@ -52,100 +66,5 @@ public class PopupBtn : MonoBehaviour
     public void OnClickReback() //잔액부족 뒤로가기 버튼을 눌렀을때
     {
         PupupError.SetActive(false);
-    }
-    
-    public void DepositAmount(int amount)  //입금
-    {
-        int money = int.Parse(GameManager.Instance.moneyText.text.Replace(",", ""));  //마지막 문구는 문자열 쉼표제거
-        int coin = int.Parse(GameManager.Instance.coinText.text.Replace(",", ""));
-        
-        if (money >= amount) 
-        {
-            money -= amount;
-            coin += amount;
-
-
-            GameManager.Instance.userData.money = money;
-            GameManager.Instance.userData.coin = coin;
-            Debug.Log("입금 되었습니다.");
-            
-            UserManager.Instance.SaveData();
-            GameManager.Instance.Refresh();
-        }
-        else
-        {
-            PupupError.SetActive(true);
-            Debug.Log("잔액이 부족합니다.");
-        }
-    }
-    
-    
-    public void CostomDeposit() //자유입금
-    {
-        int isinputDeposit = int.Parse(inputDeposit.text);
-
-        if (GameManager.Instance.userData.money >= isinputDeposit)
-        {
-            GameManager.Instance.userData.money -= isinputDeposit;
-            GameManager.Instance.userData.coin += isinputDeposit;
-            Debug.Log("자유입금 되었습니다.");
-            
-            UserManager.Instance.SaveData();
-            GameManager.Instance.Refresh();
-        }
-
-        else
-        {
-            PupupError.SetActive(true);
-            Debug.Log("잔액이 부족합니다.");
-        }
-    }
-    
-    
-    public void WithdrawalAmount(int amount) //출금
-    {
-        int money = int.Parse(GameManager.Instance.moneyText.text.Replace(",", ""));
-        int coin = int.Parse(GameManager.Instance.coinText.text.Replace(",", ""));
-
-        if (coin >= amount)
-        {
-            money += amount;
-            coin -= amount;
-            
-            GameManager.Instance.userData.coin = coin;
-            GameManager.Instance.userData.money = money;
-            Debug.Log("출금 되었습니다.");
-            
-            UserManager.Instance.SaveData();
-            GameManager.Instance.Refresh();
-            
-        }
-        else
-        {
-            PupupError.SetActive(true);
-            Debug.Log("잔액이 부족합니다.");
-        }
-    }
-    
-    public void CostomWithdrawal() //자유 출금
-    {
-        int isinputWithdrawal = int.Parse(inputWithdrawal.text);
-        if (GameManager.Instance.userData.coin >= isinputWithdrawal)
-        {
-            GameManager.Instance.userData.money += isinputWithdrawal;
-            GameManager.Instance.userData.coin -= isinputWithdrawal;
-            Debug.Log("자유출금 되었습니다.");
-            
-            UserManager.Instance.SaveData();
-            GameManager.Instance.Refresh();
-            
-        }
-
-        else
-        {
-            
-            PupupError.SetActive(true);
-            Debug.Log("잔액이 부족합니다.");
-        }
     }
 }
